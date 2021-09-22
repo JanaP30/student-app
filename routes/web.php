a0be351789fef53a715c8beb5e7f7fcdc4c0aa4e
zarko13 <zarsarenac@gmail.com>
2021-09-22 20:28:58 +0200

    landing route

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-/*
+
 Route::get('/', function () {
     return view('Welcome');
 });
-*/
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get("/success",[App\Http\Controllers\PublicController::class, 'success'])->name('success');
